afa52f90952f6d0d455b3b176e8d3cb850867168
Ian F <ian.ferg36@gmail.com>
2021-04-16 13:17:03 -0600

    1st attempt at api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+const { response } = require("express");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (app) => {
+  app.get("api/notes", (req, res) =>
+    fs.readfile(path.join(__dirname, "./db.db.json"), "utf8", (error, data) => {
+      res.json(data);
+    })
+  );
+  app.post("api/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "/db.db.json"))
+  );
+};
--- server.js
+++ server.js
@@ -6,12 +6,7 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/notes.html"))
-);
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/index.html"))
-);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlroutes")(app);
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
