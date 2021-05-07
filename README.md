4812c80bf4fa29b8b840e35958ad783a627d97ba
Ian F <ian.ferg36@gmail.com>
2021-05-07 11:51:24 -0600

    fixed the api routes after a ton of trouble shooting

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # I-take-a-the-notes
+
+# Description
+
+This app allows you to take notes
+
+# Link
+
+![Website](https://secret-ocean-94168.herokuapp.com/)
+
+# Screenshot
+
+(./public/assets/localhost_3001_notes.png)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,37 @@
 const { res } = require("express");
 const fs = require("fs");
 const path = require("path");
-
+let db = require("../db/db.json");
 module.exports = (app) => {
-  app.get("./api/notes", (req, res) =>
-    fs.readFile(path.join(__dirname, "./db.db.json"), "utf8", (error, data) => {
-      res.json(data);
-    })
-  );
-  app.post("./api/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, "./db.db.json"))
-  );
+  app.get("/api/notes", (req, res) => {
+    // const newNote = req.param.notes;
+    // res.json(newNote);
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json("There was an error reading your data");
+      }
+      db = JSON.parse(data);
+      console.log(db);
+      res.json(db);
+    });
+  });
+
+  app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        return res.send("There was an error reading your data");
+      }
+      db = JSON.parse(data);
+      const newNote = { ...req.body, id: db.length };
+      db.push(newNote);
+
+      fs.writeFile("./db/db.json", JSON.stringify(db), "utf8", (err) => {
+        if (err) {
+          return res.send("There was an error writing your data");
+        }
+        res.json(newNote);
+      });
+    });
+  });
 };
